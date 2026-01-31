4fcfdcfe3e2a23a3f959681749d7b018d7ae69d9
Naotsugu Kimura <naotsugu.kimura@gmail.com>
2026-01-31 14:34:41 +0900

    fix: CSP eval errors

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   // ここに設定を追加します
   webpack: (config) => {
     config.devtool = 'source-map';
