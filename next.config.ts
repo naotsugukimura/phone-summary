4e5dde1df0eab4af873664ee508e20eea738a449
Naotsugu Kimura <naotsugu.kimura@gmail.com>
2026-01-31 14:37:59 +0900

    fix: disable eval in webpack to solve CSP error

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // ここに設定を追加します
   webpack: (config) => {
     config.devtool = 'source-map';
-    return config;
+    return config; // ← これがないと動きません
   },
 };
 
